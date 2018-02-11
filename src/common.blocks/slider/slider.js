6de61b1f8f24e281f4ceeb6fac89c4fb73bfb2a9
PALAMATOR <palamator@ya.ru>
2018-02-11 16:34:34 +1000

    Add slider base

--- src/common.blocks/slider/slider.js
+++ src/common.blocks/slider/slider.js
@@ -3,33 +3,31 @@
   var slider__box = slider.children[0];
   var slider__flag = slider__box.children[0];
 
-  slider__flag.onmousedown = function(doc){
-    var slider__flag_coords_start = getCoords(slider__flag);
-    var slider__flag_position_shift = doc.pageX - slider__flag_coords_start.left;
-    var slider__box_coords = getCoords(slider__box);
-
+  slider__box.onmousedown = function(doc){
+    var slider__flag_position_end = doc.pageX - slider__box.getBoundingClientRect().left - slider__flag.clientWidth/2;
+    if (doc.pageX > slider__box.getBoundingClientRect().right - slider__flag.clientWidth/2) slider__flag_position_end = slider__box.clientWidth - slider__flag.clientWidth;
+    if (doc.pageX < slider__box.getBoundingClientRect().left + slider__flag.clientWidth/2) slider__flag_position_end = 0;
+    slider__flag.style.left = slider__flag_position_end + 'px';
     document.onmousemove = function(doc){
-      var slider__flag_position_end = doc.pageX - slider__flag_position_shift - slider__box_coords.left;
-      var slider__box_rightEdge = slider__box.offsetWidth - slider__flag.offsetWidth;
-
-      if (slider__flag_position_end < 0) slider__flag_position_end = 0;
-      if (slider__flag_position_end > slider__box_rightEdge) slider__flag_position_end = slider__box_rightEdge;
-
-      slider__flag.style.left = slider__flag_position_end + 'px';
-    }
-    slider__flag.style.background = "green";
-
+      var slider__flag_position_end = doc.pageX - slider__box.getBoundingClientRect().left - slider__flag.clientWidth/2;
+      if (doc.pageX > slider__box.getBoundingClientRect().right - slider__flag.clientWidth/2) slider__flag_position_end = slider__box.clientWidth - slider__flag.clientWidth;
+      if (doc.pageX < slider__box.getBoundingClientRect().left + slider__flag.clientWidth/2) slider__flag_position_end = 0;
+      slider__flag.style.left = slider__flag_position_end + 'px'
+    };
     document.onmouseup = function() {
       document.onmousemove = document.onmouseup = null;
-    };
-
-    return false;
+    }
   };
 
-  function getCoords(element){
-    var coords = element.getBoundingClientRect();
-    return {
-      left: coords.left + pageXOffset
+  slider__flag.onmousedown = function(doc){
+    document.onmousemove = function(doc){
+      var slider__flag_position_end = doc.pageX - slider__box.getBoundingClientRect().left - slider__flag.clientWidth/2;
+      if (doc.pageX > slider__box.getBoundingClientRect().right - slider__flag.clientWidth/2) slider__flag_position_end = slider__box.clientWidth - slider__flag.clientWidth;
+      if (doc.pageX < slider__box.getBoundingClientRect().left + slider__flag.clientWidth/2) slider__flag_position_end = 0;
+      slider__flag.style.left = slider__flag_position_end + 'px'
+    };
+    document.onmouseup = function() {
+      document.onmousemove = document.onmouseup = null;
     }
   }
 })();
