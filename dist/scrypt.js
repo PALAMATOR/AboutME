d5602d57e580bcd120981cee06939d5372fd89a6
PALAMATOR <palamator@ya.ru>
2018-02-15 17:27:51 +1000

    Revert "start project"
    This reverts commit 0b4346b8204b3cdb4a21d65854f2bfde2a6daa47.

--- dist/scrypt.js
+++ dist/scrypt.js
@@ -3,14 +3,6 @@
   var slider__box = slider.children[0];
   var slider__flag = slider__box.children[0];
 
-  var slider__value = document.getElementById('slider__value');
-
-  var slider__value_items = slider__value.getElementsByTagName('div').length;
-  for (var i = 0; i < slider__value_items; i++){
-    // Здесь начало реализации шага слайдера по значениям
-  }
-
-
   slider__box.onmousedown = function(doc){
     var slider__flag_position_end = doc.pageX - slider__box.getBoundingClientRect().left - slider__flag.clientWidth/2;
     if (doc.pageX > slider__box.getBoundingClientRect().right - slider__flag.clientWidth/2) slider__flag_position_end = slider__box.clientWidth - slider__flag.clientWidth;
@@ -21,10 +13,19 @@
       if (doc.pageX > slider__box.getBoundingClientRect().right - slider__flag.clientWidth/2) slider__flag_position_end = slider__box.clientWidth - slider__flag.clientWidth;
       if (doc.pageX < slider__box.getBoundingClientRect().left + slider__flag.clientWidth/2) slider__flag_position_end = 0;
       slider__flag.style.left = slider__flag_position_end + 'px'
+    };
+    document.onmouseup = function() {
+      document.onmousemove = document.onmouseup = null;
     }
-    slider__flag.ondragstart = function(){
-      return false;
-    }
+  };
+
+  slider__flag.onmousedown = function(doc){
+    document.onmousemove = function(doc){
+      var slider__flag_position_end = doc.pageX - slider__box.getBoundingClientRect().left - slider__flag.clientWidth/2;
+      if (doc.pageX > slider__box.getBoundingClientRect().right - slider__flag.clientWidth/2) slider__flag_position_end = slider__box.clientWidth - slider__flag.clientWidth;
+      if (doc.pageX < slider__box.getBoundingClientRect().left + slider__flag.clientWidth/2) slider__flag_position_end = 0;
+      slider__flag.style.left = slider__flag_position_end + 'px'
+    };
     document.onmouseup = function() {
       document.onmousemove = document.onmouseup = null;
     }
